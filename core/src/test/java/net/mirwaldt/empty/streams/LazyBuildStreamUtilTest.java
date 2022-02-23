5bc8d05316d963e634d020432cb8c84ae129f4ef
mmirwaldt <michael.mirwaldt@gmail.com>
2022-02-23 15:34:57 +0100

    added tests for parallel flag

--- core/src/test/java/net/mirwaldt/empty/streams/LazyBuildStreamUtilTest.java
+++ core/src/test/java/net/mirwaldt/empty/streams/LazyBuildStreamUtilTest.java
@@ -8,12 +8,12 @@ import java.util.stream.IntStream;
 import java.util.stream.LongStream;
 import java.util.stream.Stream;
 
+import static net.mirwaldt.empty.streams.LazyBuildStreamFactory.*;
 import static net.mirwaldt.empty.streams.util.LazyBuildDoubleStreamUtil.EMPTY_DOUBLE_STREAM_SUPPLIER;
 import static net.mirwaldt.empty.streams.util.LazyBuildIntStreamUtil.EMPTY_INT_STREAM_SUPPLIER;
 import static net.mirwaldt.empty.streams.util.LazyBuildLongStreamUtil.EMPTY_LONG_STREAM_SUPPLIER;
 import static net.mirwaldt.empty.streams.util.LazyBuildGenericStreamUtil.*;
-import static org.junit.jupiter.api.Assertions.assertNotSame;
-import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class LazyBuildStreamUtilTest {
     @Test
@@ -50,5 +50,61 @@ public class LazyBuildStreamUtilTest {
 
         var y = (Supplier<DoubleStream>) DoubleStream::empty;
         assertNotSame(EMPTY_DOUBLE_STREAM_SUPPLIER, y);
+    }
+
+    @Test
+    void testParallelGenericStreams() {
+        assertFalse(lazyBuildGenericStream(Stream.empty()).isParallel());
+        assertFalse(lazyBuildGenericStream(Stream.empty().sequential()).isParallel());
+        assertTrue(lazyBuildGenericStream(Stream.empty().parallel()).isParallel());
+        assertFalse(lazyBuildGenericStream(Stream.empty().parallel().sequential()).isParallel());
+        assertTrue(lazyBuildGenericStream(Stream.empty().sequential().parallel()).isParallel());
+
+        assertFalse(lazyBuildGenericStream(Stream.empty().sequential()).isParallel());
+        assertTrue(lazyBuildGenericStream(Stream.empty()).parallel().isParallel());
+        assertTrue(lazyBuildGenericStream(Stream.empty().sequential().parallel()).isParallel());
+        assertFalse(lazyBuildGenericStream(Stream.empty()).parallel().sequential().isParallel());
+    }
+
+    @Test
+    void testParallelIntStreams() {
+        assertFalse(lazyBuildIntStream(IntStream.empty()).isParallel());
+        assertFalse(lazyBuildIntStream(IntStream.empty().sequential()).isParallel());
+        assertTrue(lazyBuildIntStream(IntStream.empty().parallel()).isParallel());
+        assertFalse(lazyBuildIntStream(IntStream.empty().parallel().sequential()).isParallel());
+        assertTrue(lazyBuildIntStream(IntStream.empty().sequential().parallel()).isParallel());
+
+        assertFalse(lazyBuildIntStream(IntStream.empty().sequential()).isParallel());
+        assertTrue(lazyBuildIntStream(IntStream.empty()).parallel().isParallel());
+        assertTrue(lazyBuildIntStream(IntStream.empty().sequential().parallel()).isParallel());
+        assertFalse(lazyBuildIntStream(IntStream.empty()).parallel().sequential().isParallel());
+    }
+
+    @Test
+    void testParallelLongStreams() {
+        assertFalse(lazyBuildLongStream(LongStream.empty()).isParallel());
+        assertFalse(lazyBuildLongStream(LongStream.empty().sequential()).isParallel());
+        assertTrue(lazyBuildLongStream(LongStream.empty().parallel()).isParallel());
+        assertFalse(lazyBuildLongStream(LongStream.empty().parallel().sequential()).isParallel());
+        assertTrue(lazyBuildLongStream(LongStream.empty().sequential().parallel()).isParallel());
+
+        assertFalse(lazyBuildLongStream(LongStream.empty().sequential()).isParallel());
+        assertTrue(lazyBuildLongStream(LongStream.empty()).parallel().isParallel());
+        assertTrue(lazyBuildLongStream(LongStream.empty().sequential().parallel()).isParallel());
+        assertFalse(lazyBuildLongStream(LongStream.empty()).parallel().sequential().isParallel());
+    }
+
+    @Test
+    void testParallelDoubleStreams() {
+        assertFalse(lazyBuildDoubleStream(DoubleStream.empty()).isParallel());
+        assertFalse(lazyBuildDoubleStream(DoubleStream.empty().sequential()).isParallel());
+        assertTrue(lazyBuildDoubleStream(DoubleStream.empty().parallel()).isParallel());
+        assertFalse(lazyBuildDoubleStream(DoubleStream.empty().parallel().sequential()).isParallel());
+        assertTrue(lazyBuildDoubleStream(DoubleStream.empty().sequential().parallel()).isParallel());
+
+        assertFalse(lazyBuildDoubleStream(DoubleStream.empty().sequential()).isParallel());
+        assertTrue(lazyBuildDoubleStream(DoubleStream.empty()).parallel().isParallel());
+        assertTrue(lazyBuildDoubleStream(DoubleStream.empty().sequential().parallel()).isParallel());
+        assertFalse(lazyBuildDoubleStream(DoubleStream.empty()).parallel().sequential().isParallel());
     }
 }
